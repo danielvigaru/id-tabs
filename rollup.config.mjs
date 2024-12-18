fe3fbdf352d984d698cbd8e5eb56ca00cb3e4211
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2024-12-18 22:36:08 +0200

    chore: package with rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,17 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'lib/index.mjs',
+            format: 'es',
+        },
+        {
+            file: 'lib/index.umd.js',
+            format: 'umd',
+            name: 'IdTabs',
+        },
+    ],
+    plugins: [typescript()],
+};
